66585566e9208324489adb136dfd4b396d99e878
Maxim Banaev <Maxim.Banaev@jetbrains.com>
2022-07-15 15:07:01 +0200

    Initial commit

---
+++ types/variable.cpp
@@ -0,0 +1,133 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "modernize-avoid-bind"
+#pragma ide diagnostic ignored "performance-unnecessary-copy-initialization"
+#pragma ide diagnostic ignored "OCUnusedGlobalDeclarationInspection"
+#pragma ide diagnostic ignored "UnusedLocalVariable"
+
+#include <string>
+#include <vector>
+#include <array>
+#include <map>
+#include <memory>
+#include <tuple>
+
+void test_basic_types() {
+    int i = 1;
+    auto aInt = i; // int
+
+    float f = 2;
+    auto aFlat = f; // float
+
+    const double d = 3;
+    auto aDouble = d; // double
+
+    auto aChar = "char"; // const char *
+
+    bool b = true;
+    auto aBool = b; // bool // Obvious type hint should be enabled
+}
+
+template<typename T>
+void test_template_parameter_type(T t) {
+    auto at = t; // T
+}
+
+class myClass {
+};
+
+myClass getClass();
+
+void test_custom_class() {
+    auto mc = getClass(); // myClass
+}
+
+
+void test_pointers() {
+
+    int * pInt = nullptr;
+    auto aInt = pInt; // int*
+
+    int ** ppInt = nullptr;
+    auto aIInt = ppInt; // int**
+
+    const int * pcInt = nullptr;
+    auto apcInt = pcInt; // const int*
+
+    int * const cpInt = nullptr;
+    auto acpInt = cpInt; // int*
+
+    const char * const * pChar = nullptr;
+    auto apChar = pChar; // const char *const*
+
+    bool b[] = {true, false};
+    auto apBool = b; // bool *
+
+    auto aNull = nullptr; // nullptr_t
+}
+
+void test_string() {
+    std::string s("foo");
+    auto aString = s; // string
+
+    auto wStr = std::to_wstring(2137LL); // wstring
+}
+
+
+void test_std_type() {
+    std::vector v = {1, 2, 3};
+    auto aVec = v; // vector<int>
+
+    std::array a = {1.0, 2.0, 3.0};
+    auto aArr = a; // array<double, 3>
+
+    std::map<char, bool> m = {{'c', true}};
+    auto aMap = m; // map<char, bool>
+
+    const auto tuple = std::make_tuple(1, 'a', 2.3); // const tuple<int, char, double>
+}
+
+
+void test_structured_bindings() {
+    int a = 1, b = 2;
+    const auto&[x, // int&
+                y  // int&
+                ] = std::tie(a, b);
+
+    const auto& [i, // const int
+                 c, // const char
+                 d // const double
+                 ] = std::make_tuple(1, 'a', 2.3);
+}
+
+
+template<class T>
+void test_iterator(typename T::iterator Iter) {
+    auto X = Iter; // T::iterator
+}
+
+
+void test_smart_ptr() {
+    auto pShared = std::make_shared<myClass>(); // shared_ptr<myClass>
+    auto pUnique = std::make_unique<myClass>(); // unique_ptr<myClass>
+}
+
+
+void test_loop() {
+    std::vector v = {1, 2, 3};
+    for (const auto &item: v) { // int const&
+
+    }
+
+    std::map<char, bool> m = {{'c', true}};
+    for (auto &&item: m) {  // pair<const char, bool> &
+
+    }
+}
+
+int add(int x, int y) { return x + y; }
+void test_bind_to_lambda(int num) {
+    int x = 2;
+    auto clj = std::bind(add, x, num); // __bind<int (&)(int, int), int&, int&>
+}
+
+#pragma clang diagnostic pop
